6a0838d2eeb57b831806b70d843629a81627933e
timqian <timqian@t9t.io>
2022-12-19 19:12:29 +0800

    Update contract address, add metadata json

--- contract/index.js
+++ contract/index.js
@@ -1,6 +1,7 @@
 import abi from "./abi.json";
 
 export default {
-	abi,
-	address: "0xa9498a8b88eae09bfdd7a8fe4dec39b4d172036a"
-}
+  abi,
+  // address: "0xa9498a8b88eae09bfdd7a8fe4dec39b4d172036a"
+  address: "0x2093c652baeb79f14d773eed36266258f467d3fc",
+};
---
+++ pages/api/member/[id].js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  const { id } = req.query;
+	res.json({
+		"description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
+		"external_url": `https://openseacreatures.io/${id}`, 
+		"image": "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png", 
+		"name": "Sponsor of Tim Qian",
+		// "attributes": [ ... ]
+	});
+  // res.end(`Post: ${id}`);
+}
