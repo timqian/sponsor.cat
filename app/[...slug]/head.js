dfcfb6df5a795f0d6d438469ef5c0d113170ba04
timqian <timqian@t9t.io>
2023-01-03 11:55:19 +0800

    update head

--- app/[...slug]/head.js
+++ app/[...slug]/head.js
@@ -10,11 +10,11 @@ export default function Head({ params }) {
       <meta name="description" content="A decentralized funding platform. Powered by NFTs." />
       <meta name="og:description" content="A decentralized funding platform. Powered by NFTs." />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:image" content={`https://effigy.im/a/${address}.png`} />
+      <meta name="twitter:image" content={`https://effigy.im/a/${creator}.png`} />
       <meta name="twitter:site:domain" content="sponsor.cat" />
       <meta name="twitter:url" content="https://sponsor.cat" />
       <meta name="og:title" content={`${creator} - Sponsor Cat`} />
-      <meta name="og:image" content={`https://effigy.im/a/${address}.png`} />
+      <meta name="og:image" content={`https://effigy.im/a/${creator}.png`} />
       <meta name="apple-mobile-web-app-title" content="Sponsor.Cat" />
     </>
   );
