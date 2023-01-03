1a3cc9fe7f8f26d62d73b505568e7a4e610f37b5
timqian <timqian@t9t.io>
2023-01-03 11:51:55 +0800

    Update heading

--- app/[...slug]/head.js
+++ app/[...slug]/head.js
@@ -6,6 +6,16 @@ export default function Head({ params }) {
     <>
       <title>{`${creator} - Sponsor Cat`}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content="A decentralized funding platform. Powered by NFTs." />
+      <meta name="og:description" content="A decentralized funding platform. Powered by NFTs." />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:image" content={`https://effigy.im/a/${address}.png`} />
+      <meta name="twitter:site:domain" content="sponsor.cat" />
+      <meta name="twitter:url" content="https://sponsor.cat" />
+      <meta name="og:title" content={`${creator} - Sponsor Cat`} />
+      <meta name="og:image" content={`https://effigy.im/a/${address}.png`} />
+      <meta name="apple-mobile-web-app-title" content="Sponsor.Cat" />
     </>
   );
 }
--- app/head.js
+++ app/head.js
@@ -4,14 +4,14 @@ export default function Head() {
       <title>Sponsor.cat</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="" />
+      <meta name="description" content="A decentralized funding platform. Powered by NFTs." />
       <meta name="og:description" content="A decentralized funding platform. Powered by NFTs." />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://sponsor.cat/og2.png" />
+      <meta name="twitter:image" content="https://sponsor.cat/og.png" />
       <meta name="twitter:site:domain" content="sponsor.cat" />
       <meta name="twitter:url" content="https://sponsor.cat" />
       <meta name="og:title" content="Sponsor Cat" />
-      <meta name="og:image" content="https://sponsor.cat/og2.png" />
+      <meta name="og:image" content="https://sponsor.cat/og.png" />
       <meta name="apple-mobile-web-app-title" content="Sponsor.Cat" />
       <link
         rel="apple-touch-icon"
